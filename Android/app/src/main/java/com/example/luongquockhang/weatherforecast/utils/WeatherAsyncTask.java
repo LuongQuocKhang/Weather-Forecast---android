91556733fdec392bb7b458c6228b8c7dded0a6d3
Quoc khang Luong <khanghuu97@gmail.com>
2017-11-04 19:42:15 +0700

    Cập nhật 4/11/2017
    - cập nhật giao diện navigationview , mainactivity

--- Android/app/src/main/java/com/example/luongquockhang/weatherforecast/utils/WeatherAsyncTask.java
+++ Android/app/src/main/java/com/example/luongquockhang/weatherforecast/utils/WeatherAsyncTask.java
@@ -1,10 +1,7 @@
 package com.example.luongquockhang.weatherforecast.utils;
 
+import android.app.Activity;
 import android.os.AsyncTask;
-import android.os.Bundle;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
 import android.widget.TextView;
 
 import com.example.luongquockhang.weatherforecast.Model.OpenWeatherJSON;
@@ -12,12 +9,19 @@ import com.example.luongquockhang.weatherforecast.R;
 
 import org.json.JSONException;
 
+import java.util.Date;
+
 /**
  * Created by Luong Quoc Khang on 10/27/2017.
  */
 
 public class WeatherAsyncTask extends AsyncTask<String,String,OpenWeatherJSON>
 {
+    Activity activity;
+    public WeatherAsyncTask(Activity activity)
+    {
+        this.activity = activity;
+    }
 
     @Override
     protected OpenWeatherJSON doInBackground(String... params) {
@@ -25,7 +29,6 @@ public class WeatherAsyncTask extends AsyncTask<String,String,OpenWeatherJSON>
         WeatherHttpClient http = new WeatherHttpClient();
         String data = http.getWeatherData(params[0]);
 
-        OpenWeatherMapAPI object = new OpenWeatherMapAPI();
         OpenWeatherJSON JObject = null;
         try {
             JObject = OpenWeatherMapAPI.getWeather(data);
@@ -40,5 +43,30 @@ public class WeatherAsyncTask extends AsyncTask<String,String,OpenWeatherJSON>
     protected void onPostExecute(OpenWeatherJSON openWeatherJSON) {
         super.onPostExecute(openWeatherJSON);
 
+        TextView CurrentLocation,Temperature,Max_temp,Min_temp,Wind,Pressure,Humidity,Clouds,Sunrise,Description;;
+        CurrentLocation = (TextView)activity.findViewById(R.id.textViewLocation);
+        Temperature = (TextView)activity.findViewById(R.id.textViewTemp);
+        Max_temp = (TextView)activity.findViewById(R.id.textViewMaxtemp);
+        Min_temp = (TextView)activity.findViewById(R.id.textViewMinTemp);
+        Wind = (TextView)activity.findViewById(R.id.textViewWind);
+        Pressure = (TextView)activity.findViewById(R.id.textViewPressure);
+        Humidity = (TextView)activity.findViewById(R.id.textViewHumidity);
+        Clouds = (TextView)activity.findViewById(R.id.textViewClouds);
+        Sunrise = (TextView)activity.findViewById(R.id.textViewSunrise);
+        Description = (TextView)activity.findViewById(R.id.textviewDescription);
+
+        CurrentLocation.setText(openWeatherJSON.getName());
+        Temperature.setText( String.valueOf(openWeatherJSON.getMain().getTemp()) + "°C");
+        Max_temp.setText(String.valueOf(openWeatherJSON.getMain().getTemp_max()) + "°C");
+        Min_temp.setText(String.valueOf(openWeatherJSON.getMain().getTemp_min()) + "°C");
+        Wind.setText(String.valueOf(openWeatherJSON.getWind().getSpeed()) + "m/s");
+        Pressure.setText(String.valueOf(openWeatherJSON.getMain().getPressure()) + "hpa");
+        Humidity .setText(String.valueOf(openWeatherJSON.getMain().getHumidity()) + "%");
+        Clouds.setText(openWeatherJSON.getWeather().getMain());
+
+        Date TimeSunrise = new Date((long) openWeatherJSON.getSys().getSunrise());
+        String Time = TimeSunrise.getHours() + " : " + TimeSunrise.getMinutes() + " AM";
+        Sunrise.setText(Time);
+        Description.setText(openWeatherJSON.getWeather().getDesciption());
     }
 }
--- Android/app/src/main/java/com/example/luongquockhang/weatherforecast/weather_information.java
+++ Android/app/src/main/java/com/example/luongquockhang/weatherforecast/weather_information.java
@@ -1,85 +1,20 @@
 package com.example.luongquockhang.weatherforecast;
 
-import android.annotation.SuppressLint;
-import android.content.Intent;
-import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.TextView;
 
-import com.example.luongquockhang.weatherforecast.Model.OpenWeatherJSON;
-import com.example.luongquockhang.weatherforecast.utils.OpenWeatherMapAPI;
-import com.example.luongquockhang.weatherforecast.utils.WeatherHttpClient;
+import com.example.luongquockhang.weatherforecast.utils.WeatherAsyncTask;
 
-import org.json.JSONException;
-
-import java.util.Date;
 
 public class weather_information extends AppCompatActivity {
 
-    TextView CurrentLocation,Temperature,Max_temp,Min_temp,Wind,Pressure,Humidity,Clouds,Sunrise,Description;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_weather_information);
-
-
-        CurrentLocation = (TextView)findViewById(R.id.textViewLocation);
-        Temperature = (TextView)findViewById(R.id.textViewTemp);
-        Max_temp = (TextView)findViewById(R.id.textViewMaxtemp);
-        Min_temp = (TextView)findViewById(R.id.textViewMinTemp);
-        Wind = (TextView)findViewById(R.id.textViewWind);
-        Pressure = (TextView)findViewById(R.id.textViewPressure);
-        Humidity = (TextView)findViewById(R.id.textViewHumidity);
-        Clouds = (TextView)findViewById(R.id.textViewClouds);
-        Sunrise = (TextView)findViewById(R.id.textViewSunrise);
-        Description = (TextView)findViewById(R.id.textviewDescription);
     }
     protected void onResume() {
 
         super.onResume();
-        Intent intent = getIntent();
-        String data = intent.getStringExtra("data");
-        WeatherTask Task = new WeatherTask();
-        Task.execute(data);
-    }
-
-    private class WeatherTask extends AsyncTask<String,String,OpenWeatherJSON>
-    {
-        @Override
-        protected OpenWeatherJSON doInBackground(String... params) {
-
-            WeatherHttpClient http = new WeatherHttpClient();
-            String data = http.getWeatherData(params[0]);
-
-            OpenWeatherJSON JObject = null;
-            try {
-                JObject = OpenWeatherMapAPI.getWeather(data);
-                return JObject;
-            } catch (JSONException e) {
-                e.printStackTrace();
-            }
-            return null;
-        }
-
-        @SuppressLint("SetTextI18n")
-        @Override
-        protected void onPostExecute(OpenWeatherJSON openWeatherJSON) {
-            super.onPostExecute(openWeatherJSON);
-
-            CurrentLocation.setText(openWeatherJSON.getName());
-            Temperature.setText( String.valueOf(openWeatherJSON.getMain().getTemp()) + "°C");
-            Max_temp.setText(String.valueOf(openWeatherJSON.getMain().getTemp_max()) + "°C");
-            Min_temp.setText(String.valueOf(openWeatherJSON.getMain().getTemp_min()) + "°C");
-            Wind.setText(String.valueOf(openWeatherJSON.getWind().getSpeed()) + "m/s");
-            Pressure.setText(String.valueOf(openWeatherJSON.getMain().getPressure()) + "hpa");
-            Humidity .setText(String.valueOf(openWeatherJSON.getMain().getHumidity()) + "%");
-            Clouds.setText(openWeatherJSON.getWeather().getMain());
-
-            Date TimeSunrise = new Date((long) openWeatherJSON.getSys().getSunrise());
-            String Time = TimeSunrise.getHours() + " : " + TimeSunrise.getMinutes() + " AM";
-            Sunrise.setText(Time);
-            Description.setText(openWeatherJSON.getWeather().getDesciption());
-        }
     }
 }
